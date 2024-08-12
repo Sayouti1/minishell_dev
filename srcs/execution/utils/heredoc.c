29b942100ff2fb6f7aef7984d5d26f9b8cd5f8ce
aes-sayo-DellG3 <aes-sayo@student.1337.ma>
2024-08-12 08:39:18 +0100

    SIGQUIT ignored in heredoc -

--- srcs/execution/utils/heredoc.c
+++ srcs/execution/utils/heredoc.c
@@ -76,6 +76,7 @@ int	open_heredoc(t_redirection *redirection)
 	del = char_concat(redirection->file_name, '\n');
 	//free(cmd->redirection->file_name)
 	redirection->file_name = NULL;
+	signal(SIGQUIT, SIG_IGN);
 	while (1)
 	{
 		byte_read = read(0, buff, 1000);
