8c466ca027190cee746d91e3bd0cdfc36b2f4eab
aes-sayo-DellG3 <aes-sayo@student.1337.ma>
2024-08-23 23:04:49 +0100

    heredoc mod

--- srcs/execution/utils/heredoc.c
+++ srcs/execution/utils/heredoc.c
@@ -45,7 +45,7 @@ int	check_curly_braces(char *str)
 	int	i;
 
 	if (NULL == str)
-		return (1);
+		return (0);
 	i = 0;
 	while (str[i])
 	{
@@ -84,7 +84,7 @@ int	open_heredoc(t_redirection *redirection)
 
 	del = char_concat(redirection->file_name, '\n');
 	redirection->file_name = NULL;
-	signal(SIGQUIT, SIG_IGN);
+	signal(SIGQUIT, ignore_sig);
 	while (1)
 		if (read_and_join(buff, del, redirection))
 			break ;
@@ -100,3 +100,50 @@ int	open_heredoc(t_redirection *redirection)
 	redirection->fd = fds[0];
 	return (dup2(redirection->fd, 0), 0);
 }
+
+// int	open_heredoc(t_redirection *redirection)
+// {
+// 	char	*line;
+// 	char	*del;
+// 	int		fds[2];
+// 	char	*text;
+// 	// int		sub_var;
+
+// 	// sub_var = 0;
+// 	// if (!ft_char_in('\'', redirection->file_name)
+// 		// && !ft_char_in('"', redirection->file_name))
+// 		// sub_var = 1;
+// 	del = remove_all_quotes(redirection->file_name);
+// 	redirection->file_name = NULL;
+// 	signal(SIGQUIT, ignore_sig);
+// 	pipe(fds);
+// 	text = NULL;
+// 	while (1)
+// 	{
+// 		line = readline(">");
+// 		if (NULL == line)
+// 			break ;
+// 		if (!ft_strcmp(line, del))
+// 		{
+// 			free(line);
+// 			break;
+// 		}
+// 		text = ft_strjoin_gnl(text,);
+// 		// if (check_curly_braces(line))
+// 		// 	return (set_exit_status(1), printf(" : bad substitution\n"), 1);
+// 		ft_putstr_fd(line, fds[1]);
+// 		ft_putchar_fd('\n', fds[1]);
+// 		free(line);
+// 	}
+// 	free(del);
+// 	// if (!ft_char_in('\'', del) && !ft_char_in('"', del))
+// 	// {
+// 	// 	if (check_curly_braces(redirection->file_name))
+// 	// 		return (set_exit_status(1), printf(" : bad substitution\n"), 1);
+// 	// 	redirection->file_name = substitute_var(redirection->file_name);
+// 	// }
+// 	// write(fds[1], redirection->file_name, ft_strlen(redirection->file_name));
+// 	close(fds[1]);
+// 	redirection->fd = fds[0];
+// 	return (dup2(redirection->fd, 0), 0);
+// }
