6342ed7e1754ae4cb245bdc11b73c5a1cf9333b5
aes-sayo <aes-sayo@student.1337.ma>
2024-09-08 22:45:05 +0100

    some leaks fixed

--- srcs/execution/utils/token_to_command_2.c
+++ srcs/execution/utils/token_to_command_2.c
@@ -18,7 +18,8 @@ char	*trim_str(char *str)
 	int		i;
 
 	i = -1;
-	tmp = ft_strdup("");
+	tmp = NULL;
+	// tmp = ft_strdup("");
 	while (str && str[++i])
 	{
 		if (str[i] == '\'')
@@ -35,7 +36,7 @@ char	*trim_str(char *str)
 		}
 		else
 			tmp = char_concat(tmp, str[i]);
-		collect_garbage(tmp);
+		// collect_garbage(tmp);
 	}
 	free(str);
 	return (tmp);
